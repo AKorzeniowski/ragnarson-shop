170ace0a80aeedde3921ac8a5349315e4dd4cc75
Schocker <adikorzeniowski@gmail.com>
2016-07-09 15:57:21 +0200

    Fix main.rb after moving lib files to a separate dir

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
-require_relative './product.rb'
-require_relative './storage.rb'
-require_relative './warehouse.rb'
-require_relative './cart.rb'
+require_relative 'lib/product.rb'
+require_relative 'lib/storage.rb'
+require_relative 'lib/warehouse.rb'
+require_relative 'lib/cart.rb'
 
 products = [Product.new("Milk", 2, 23), Product.new("Book", 10, 8), Product.new("Gems", 100, 23)]
 
@@ -10,16 +10,12 @@ ware = Warehouse.new("Main", products)
 ware.add(1, 1)
 ware.add(2, 2)
 ware.to_s
-p ware
 
 cart = Cart.new("Shopping", ware)
 cart.add(1, 1)
 cart.add(1, 2)
 cart.add(3, 1)
 cart.to_s
-p cart
 
 ware.remove(1)
 ware.to_s
-
-p cart
