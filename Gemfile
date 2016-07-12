a8f512160d45608dd22c8fceafbded5af3f0de84
Schocker <adikorzeniowski@gmail.com>
2016-07-12 12:30:48 +0200

    Add Sinatra

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'sinatra'
+
 group :test do
   gem 'guard'
   gem 'guard-rspec'
--- main.rb
+++ main.rb
@@ -1,21 +1,22 @@
-require_relative 'lib/product.rb'
-require_relative 'lib/storage.rb'
-require_relative 'lib/warehouse.rb'
-require_relative 'lib/cart.rb'
+require "bundler/setup"
+require "sinatra/base"
 
-products = [Product.new("Milk", 2, 23), Product.new("Book", 10, 8), Product.new("Gems", 100, 23)]
+Dir["./lib/**/*.rb"].each{|file| require file}
 
-ware = Warehouse.new("Main", products)
+module Store
+  PRODUCTS = [
+    Product.new("Milk", 2, 23), 
+    Product.new("Book", 10, 8), 
+    Product.new("Gems", 100, 23)
+  ]
 
-ware.add(1, 1)
-ware.add(2, 2)
-ware.to_s
+  WAREHOUSE = []
+  BASKET = []
 
-cart = Cart.new("Shopping", ware)
-cart.add(1, 1)
-cart.add(1, 2)
-cart.add(3, 1)
-cart.to_s
-
-ware.remove(1)
-ware.to_s
+  class App < Sinatra::Base
+    get "/" do
+      products = FetchProducts.new.call      
+    end
+  end
+end
+# ware.to_s
