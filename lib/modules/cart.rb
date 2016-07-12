038f88f5ab2bcbdf4689ac7568abc260647fd712
Schocker <adikorzeniowski@gmail.com>
2016-07-12 12:39:58 +0200

    Clean up cart.rb after move to microservices

--- lib/modules/cart.rb
+++ lib/modules/cart.rb
@@ -1,53 +1,24 @@
 module Store
   class Cart
-    attr_reader :id, :name, :warehouse, :content
-    
-    @@last_id = 0
+    attr_reader :id, :qty
   
-    def initialize(name, warehouse)
-      @name = name
-      @id = @@last_id += 1
-      @content = []
-      @warehouse = warehouse
+    def initialize(id, qty)
+      @id = validate(id, Numeric)
+      @qty = validate(id, Numeric)
     end
-  
-    def remove(id) 
-      super
-      warehouse.add(id)
+
+    def fetch_product 
+      FetchProduct.new.call(id)
     end
-  
-    def to_s
-      print "Koszyk #{name}\n"
-      print "id\tname\tprice\tqty\n"
-  
-      content.each do |p|
-        prod = warehouse.get_product(id)
-        print "#{p[:id]}\t#{prod.name}\t#{prod.price}\t\t#{p[:qty]}\n"
-      end
-  
-      sum(true)
-      sum(false)
+
+    def brutto_product_price
+      product = fetch_product
+      product.price * qty
     end
-    
-      private
-        def sum(brutto)
-          sum = 0
-  
-          content.each do |p|
-            product = warehouse.get_product(p[:id])
-  
-            if brutto 
-              sum += product.price_with_vat * p[:qty]
-            else
-              sum += product.price * p[:qty]
-            end
-          end
-  
-          if brutto then print "Suma brutto: #{sum}\n" else print "Suma netto: #{sum}\n" end
-        end
-  
-        def price_with_vat(price, vat)
-          (price * (1+vat/100.0)).round(2)
-        end
+
+    def brutto_product_price
+      product = fetch_product
+      product.price_with_vat * qty
+    end
   end
 end
